f57b5bf8e0ae210e48262b0d0763220ac7d30e5d
mike <mgelistat@hotmail.com>
2016-09-20 16:21:20 -0500

    prueba 1
    hola mundo

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		
+	</head>
+	<body>	
+		<!--un data rol es un atributo personalizado compatible con html5, su sintaxis es "data-" y es lo que la hace
+		platarforma de desarrollo,  -->
+		
+
+		<h1>Hola mundo</h1>
+			<!--Pie de pagina-->
+			
+		
+
+
+	</body>
+</html>
